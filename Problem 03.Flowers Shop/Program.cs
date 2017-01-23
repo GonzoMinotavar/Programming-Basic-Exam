1f7a4fa8ece76819b2071bb55028c2aad4be7ceb
GonzoMinotavar <gonzoggeorgiev98@gmail.com>
2017-01-23 18:59:34 +0200

    Added Problem 03.Flowers Shop and Problem 04.Grades

---
+++ Problem 03.Flowers Shop/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowersShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Hrizantemi = int.Parse(Console.ReadLine());
+            var Roses = int.Parse(Console.ReadLine());
+            var Laleta = int.Parse(Console.ReadLine());
+            var season = Console.ReadLine();
+            var praznik = Console.ReadLine();
+
+            var HrizantemiPrice = 0.00;
+            var rosesPrice = 0.00;
+            var LaletaPrice = 0.00;
+
+            var flowersCount = Roses + Laleta + Hrizantemi;
+            var FlowersPrice = 0.00;
+            switch (season)
+            {
+                case "Winter":
+                    HrizantemiPrice = 3.75;
+                    rosesPrice = 4.50;
+                    LaletaPrice = 4.15;
+                    break;
+
+                case "Spring":
+                    HrizantemiPrice = 2.00;
+                    rosesPrice = 4.10;
+                    LaletaPrice = 2.50;  
+                    break;
+                case "Summer":
+                    HrizantemiPrice = 2.00;
+                    rosesPrice = 4.10;
+                    LaletaPrice = 2.50;
+                    break;
+                case "Autumn":
+                    HrizantemiPrice = 3.75;
+                    rosesPrice = 4.50;
+                    LaletaPrice = 4.15;
+                    break;
+            }
+            
+
+            FlowersPrice = (Laleta * LaletaPrice) + (Roses * rosesPrice) + (Hrizantemi * HrizantemiPrice);
+
+            if (praznik == "Y")
+            {
+                FlowersPrice = FlowersPrice * 1.15;
+            }
+
+            if (season == "Spring" && Laleta > 7)
+            {
+                FlowersPrice = FlowersPrice * 0.95;
+            }
+
+
+            if (season == "Winter" && Roses >= 10)
+            {
+                FlowersPrice = FlowersPrice * 0.90;
+            }
+
+            if (flowersCount > 20)
+            {
+                FlowersPrice = FlowersPrice * 0.80;
+            }
+
+            Console.WriteLine("{0:f2}",FlowersPrice + 2); 
+           
+            
+           
+
+
+
+        }
+    }
+}
+6
---
+++ Problem 04.Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Grades
+{
+    public class Grades
+    {
+        public static void Main()
+        {
+            int students = int.Parse(Console.ReadLine());
+            double bestStudents = 0;
+            double betterStudents = 0;
+            double goodStudents = 0;
+            double failStudents = 0;
+            double average = 0;
+
+            for (int i = 0; i < students; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 5.00)
+                {
+                    bestStudents++;
+                }
+                else if (grade >= 4 && grade < 5)
+                {
+                    betterStudents++;
+                }
+                else if (grade >= 3 && grade < 4)
+                {
+                    goodStudents++;
+                }
+                else
+                {
+                    failStudents++;
+                }
+                average += grade;
+            }
+
+            Console.WriteLine("Top students: {0:F2}%", bestStudents / students * 100);
+            Console.WriteLine("Between 4.00 and 4.99: {0:F2}%", betterStudents / students * 100);
+            Console.WriteLine("Between 3.00 and 3.99: {0:F2}%", goodStudents / students * 100);
+            Console.WriteLine("Fail: {0:F2}%", failStudents / students * 100);
+            Console.WriteLine("Average: {0:F2}", average / students);
+        }
+    }
+}
